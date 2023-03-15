2b526496c6185d4b0bdf495c164d22458fcb264b
Saed Mejahid <94481351+saedmejhid@users.noreply.github.com>
2023-03-15 03:43:51 +0200

    edit admin js

--- flutter server nodejs/routes/admin.js
+++ flutter server nodejs/routes/admin.js
@@ -6,9 +6,9 @@ const Product = require('../models/product');
 adminRouter.post('/admin/add-product',admin,async(req,res) => {
     try
     {
-        const {name, description , images , qty , category} = req.body;
+        const {name,price ,description , images , qty , category} = req.body;
         let product = new Product({
-            name, description , images , qty , category
+            name,price ,description , images , qty , category
         });
         product = await product.save();
         res.json(product);
