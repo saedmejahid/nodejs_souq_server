75d78369f88b636cf432aaea0efda4afce6cb840
Saed Mejahid <94481351+saedmejhid@users.noreply.github.com>
2023-03-21 05:15:08 +0200

    up

--- flutter server nodejs/routes/admin.js
+++ flutter server nodejs/routes/admin.js
@@ -2,6 +2,7 @@ const express =require('express');
 const adminRouter = express.Router();
 const admin = require('../components/my_admin');
 const {Product} = require('../models/product');
+const Order = require('../models/order');
 
 adminRouter.post('/admin/add-product',admin,async(req,res) => {
     try
@@ -44,4 +45,23 @@ adminRouter.post('/admin/delete-products',admin, async (req,res) =>
         res.status(5000).json({error :e.meesage});
     }
 });
+adminRouter.get('/api/all-orders-admin', admin, async (req, res) => {
+    try {
+        const orders = await Order.find({});
+        res.json(orders);
+    } catch (e) {
+        res.status(500).json({error: e.message});
+    }
+  });
+  adminRouter.post('/admin/update-order-status', admin, async (req, res) => {
+    try {
+      const {id , status} = req.body;
+      let order = await Order.findById(id);
+      order.status = status;
+      order = await order.save();
+      res.json(order);
+    } catch (e) {
+      res.status(500).json({error: e.message});
+    }
+  });
 module.exports = adminRouter;
